8f45935305ff107ec37d6f55749c2514cde82d31
Felix Kemboi <felixkemboi824@gmail.com>
2025-09-27 08:24:22 +0300

    had that for presentation

--- Backend/app.py
+++ Backend/app.py
@@ -26,8 +26,8 @@ def create_app():
         resources={r"/api/*": {"origins": [
             "http://localhost:5173",
             "http://127.0.0.1:5173",
-            "http://localhost:5175",
-            "http://127.0.0.1:5175",
+            "http://localhost:5174",
+            "http://127.0.0.1:5174",
         ]}},
         supports_credentials=True
     )
